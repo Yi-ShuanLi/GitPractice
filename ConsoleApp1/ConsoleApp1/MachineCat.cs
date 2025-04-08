cc1029b6c58f565a5a02a8d45df4e5ee72711ccc
Yi-ShuanLi <beauty1997724@gmail.com>
2025-04-08 14:59:34 +0800

    新增機械貓2

---
+++ ConsoleApp1/ConsoleApp1/MachineCat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MachineCat
+    {
+        //Console.WriteLine("MachineCat")
+        //Console.WriteLine("MachineCat2")
+    }
+}
