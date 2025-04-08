93daee4b8cbcd13d24d6ab8502a191ddf63b866b
Yi-ShuanLi <beauty1997724@gmail.com>
2025-04-08 13:08:22 +0800

    合併一上傳+dogs
    dog1
    
    dog2
    
    dog3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO");
+
+            Console.WriteLine("World");
+            Console.WriteLine("!!!!!!");
+            Console.WriteLine("123");
+            Console.WriteLine("456");
+            Console.WriteLine("789");
+            Console.WriteLine("000");
+            Console.WriteLine("aaaa");
+            Console.WriteLine("bbbb");
+            Console.WriteLine("cccc");
+            Console.WriteLine("dddd");
+            Console.WriteLine("eeee");
+            Console.WriteLine("dog1");
+            Console.WriteLine("dog2");
+            Console.WriteLine("dog3");
+            Console.ReadKey();
+        }
+    }
+}
