76ba8381b78788eff78e2dcc9ef58aa54b20d1a5
Yi-ShuanLi <beauty1997724@gmail.com>
2025-04-08 14:06:02 +0800

    cat2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -28,6 +28,7 @@ namespace ConsoleApp1
             Console.WriteLine("dog3");
             Console.WriteLine("dog4");
             Console.WriteLine("cat1");
+            Console.WriteLine("cat2");
             Console.ReadKey();
         }
     }
